e114789f06036c8ecf4e01b6224da147f364a296
to7m <ThomasHoweMain@gmail.com>
2024-11-04 22:16:25 +0000

    test build 14

--- one_way_copy.py
+++ one_way_copy.py
@@ -16,7 +16,6 @@ class OwcPath:
         shutil.copy(self.path, dst_path)
 
     def delete(self):
-        print(self.path); raise
         if self.is_dir():
             for child in self.iterdir():
                 child.delete()
