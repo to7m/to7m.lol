47be24f0f8d57d265f1258edca349c79c227398c
to7m <ThomasHoweMain@gmail.com>
2024-11-17 00:54:30 +0000

    added debug code for initial press

--- src/server_template/public_html/client-runnable/theremin/main.js
+++ src/server_template/public_html/client-runnable/theremin/main.js
@@ -27,20 +27,30 @@ async function activate() {
     xProportionParam = workletNode.parameters.get("xProportion");
     yProportionParam = workletNode.parameters.get("yProportion");
     running = true;
+    console.log(`A ${running}`);
 
     for (let i = 0; i < 10; i++) {
         console.log(`x: ${xProportionParam}, y: ${yProportionParam}`);
         await sleep(0.001);
     }
+
+
+    console.log(`B ${running}`);
 }
 
 function handleOffsets(offsets) {
+
+    console.log(`F ${running}`);
     if (running) {
+
+    console.log(`G ${running}`);
         const xProportion = offsets.offsetX / (canvas.scrollWidth - 1);
         const yProportion = offsets.offsetY / (canvas.scrollHeight - 1);
         xProportionParam.setValueAtTime(xProportion, audioContext.currentTime);
         yProportionParam.setValueAtTime(clicked ? yProportion : 3.0, audioContext.currentTime);
     } else {
+
+    console.log(`H ${running}`);
         console.log("not running");
     }
 }
@@ -48,12 +58,18 @@ function handleOffsets(offsets) {
 
 canvas.addEventListener("pointerdown", async (event) => {
     if (!activated) {
+
+        console.log(`C ${running}`);
         activate();
+
+    console.log(`D ${running}`);
 
         console.log("activated");
     }
 
     clicked = true;
+
+    console.log(`E ${running}`);
     handleOffsets(event);
 
     console.log("pointerdown");
